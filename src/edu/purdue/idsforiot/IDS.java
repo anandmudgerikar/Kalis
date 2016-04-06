5f9fcaaf3591c0b249502bce87019f198f61d619
Daniele MIDI <danielemidi@gmail.com>
2016-04-06 14:07:57 -0400

    Cleanup + fixing cmd line arg parsing

--- src/edu/purdue/idsforiot/IDS.java
+++ src/edu/purdue/idsforiot/IDS.java
@@ -8,41 +8,34 @@ public class IDS {
 		
 		// parse the command line args
 		String source = null;
-		if (args.length == 2 && args[0].equals("-comm")) {
-			source = args[1];
-		} 
-		else if (args.length > 0) {
-			System.err.println("usage: java edu.purdue.idsforiot.IDS [-comm PACKETSOURCE] [-t Tracefile]");
-			System.err.println("       (default packet source from MOTECOM environment variable)");
-			System.exit(2);
-		}
+		String tracefile = null;
+		for (String a : args) {
+			if (a.startsWith("-comm=")) source = a.substring("-comm=".length());
+			else if (a.startsWith("-t=")) tracefile = a.substring("-t=".length());
+			else {
+				System.err.println("usage: java edu.purdue.idsforiot.IDS [-comm=packetsource] [-t=tracefile]");
+				System.err.println("       (default packet source from MOTECOM environment variable)");
+				System.exit(2);
+			}
+		}		
 		
-		String tracefile;
-		// TODO: read possible tracefile from command line argument. For now, it's hardcoded.
-		if(args.length == 2 && args[0].equals("-t"))
-			tracefile = args[1];	
-		else	
-			tracefile = "/home/odroid/tinyos-main/project/ids/data/CVSpacket_capture.txt";
-		
-		// create a Communicator to intercept packets
-		Communicator communicator = new Communicator(source);
-				
 		// start the ModuleManager to load the necessary modules
 		try {
 			ModuleManager.getInstance().start();
 		} catch (InstantiationException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.exit(2);
 		}
 		
 		if (tracefile.equals(null)) {
+			// create a Communicator to intercept packets
+			Communicator communicator = new Communicator(source);
+			
 			// start listening for live packets
 			communicator.listen();
-			
 		} else {
 			// replay a trace
 			DataStore.getInstance().replayTrace(tracefile);
-			
 		}
 		
 	}
